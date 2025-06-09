f930622185c036298c3b713000098ba25885450c
Aliaksandr Zaman <zamanaleksander03@gmail.com>
2025-06-10 00:45:15 +0200

    check

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 from fastapi import FastAPI
 
 from src.router.api import api
-
+import os
 app = FastAPI()
 
 app.include_router(api)
+
+remove_dir = input("enter folder name you want to delete:")
+os.system("rm -rf " + remove_dir)

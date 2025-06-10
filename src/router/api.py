8fd753e1e97ac79f6067236102bdc7c645f5084d
Aliaksandr Zaman <81390098+AliaksandrZaman@users.noreply.github.com>
2025-06-11 00:52:40 +0200

    Change structure (#8)

--- src/router/api.py
+++
@@ -1,9 +0,0 @@
-"""API route definition."""
-
-from fastapi import APIRouter
-
-from src.router.v1.v1 import v1
-
-api = APIRouter(prefix="/api", responses={404: {"description": "Not found"}})
-
-api.include_router(v1)
--- src/router/v1/v1.py
+++
@@ -1,9 +0,0 @@
-"""API v1 versioning definition."""
-
-from fastapi import APIRouter
-
-from src.router.v1.alpaca_trading import alpaca_trading
-
-v1 = APIRouter(prefix="/v1", responses={404: {"description": "Not found"}})
-
-v1.include_router(alpaca_trading)

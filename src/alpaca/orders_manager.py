77a75bb7edce79bf5fdaa47d08be02cae4156bf7
Andrzej-Tezyk <andrewtezhik@gmail.com>
2025-06-23 01:43:37 +0200

    finished asset manager

--- src/alpaca/orders_manager.py
+++ src/alpaca/orders_manager.py
@@ -4,6 +4,13 @@ from dotenv import load_dotenv
 from alpaca.trading.enums import OrderSide, QueryOrderStatus
 from alpaca.trading.models import Order
 from alpaca.trading.requests import GetOrdersRequest
+from alpaca.common import RawData
+from alpaca.trading.client import TradingClient
+
+load_dotenv()
+ALPACA_KEY = os.getenv("ALPACA_KEY")
+ALPACA_SECRET = os.getenv("ALPACA_SECRET")
+
 
 trading_client = TradingClient(ALPACA_KEY, ALPACA_SECRET, paper=True)
 

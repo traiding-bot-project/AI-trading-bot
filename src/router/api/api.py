24c393e7121843d7cb55cdf2b78d2f2e1aa03712
Aliaksandr Zaman <zamanaleksander03@gmail.com>
2025-06-23 19:46:43 +0200

    fix

--- src/router/api/api.py
+++ src/router/api/api.py
@@ -1,9 +1,13 @@
 """API route definition."""
 
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
 
 from src.router.api.v1.v1 import v1
+
+app = FastAPI()
 
 api = APIRouter(prefix="/api", responses={404: {"description": "Not found"}})
 
 api.include_router(v1)
+
+app.include_router(api)

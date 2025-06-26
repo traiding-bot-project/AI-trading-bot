01425055d8b1eafabf020df7f7422730e65ecb7e
Andrzej-Tezyk <andrewtezhik@gmail.com>
2025-06-26 21:15:44 +0200

    orders manager

--- src/alpaca/orders_manager.py
+++ src/alpaca/orders_manager.py
@@ -1,11 +1,14 @@
 import os
+from typing import Optional
 
 from dotenv import load_dotenv
-from alpaca.trading.enums import OrderSide, QueryOrderStatus
+from alpaca.trading.enums import OrderSide, QueryOrderStatus, TimeInForce
 from alpaca.trading.models import Order
 from alpaca.trading.requests import GetOrdersRequest
 from alpaca.common import RawData
 from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+
 
 load_dotenv()
 ALPACA_KEY = os.getenv("ALPACA_KEY")
@@ -18,3 +21,16 @@ def get_orders_info(status: QueryOrderStatus, side: OrderSide) -> list[Order] | 
     """Get orders info from AlpacaAPI."""
     request_params = GetOrdersRequest(status=status, side=side)
     return trading_client.get_orders(filter=request_params)
+
+
+def prepare_order(symbol: str,
+                  side, 
+                  asset_qty: Optional[float],
+                  asset_in_money: Optional[float], 
+                  time_in_force = TimeInForce.DAY
+                  ) -> :
+    market_order_request = MarketOrderRequest(
+        symbol = symbol,
+        qty = 
+        notional = 
+    )

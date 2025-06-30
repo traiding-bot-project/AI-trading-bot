cb02d0226e8e89d29950a500378c0dd19ba11cc5
Vlad Yanovich <vladislavyanovich2004@gmail.com>
2025-06-30 17:18:10 +0300

    created a decorator and add this decorator to functions, also modified functions for the decorator purposes
    Took 2 hours 10 minutes

---
+++ src/alpaca/common.py
@@ -0,0 +1,16 @@
+"""Decorator for logging in"""
+
+import os
+
+from dotenv import load_dotenv
+
+from alpaca.trading.client import TradingClient
+
+def with_alpaca_trading_client(func):
+    def wrapper(*args, **kwargs):
+        load_dotenv()
+        ALPACA_KEY = os.getenv("ALPACA_KEY")
+        ALPACA_SECRET = os.getenv("ALPACA_SECRET")
+        client = TradingClient(ALPACA_KEY, ALPACA_SECRET, paper=True)
+        return func(client, *args, **kwargs)
+    return wrapper
--- src/alpaca/trading.py
+++ src/alpaca/trading.py
@@ -3,7 +3,7 @@
 import os
 
 from dotenv import load_dotenv
-
+from common import with_alpaca_trading_client
 from alpaca.common import RawData
 from alpaca.trading.client import TradingClient
 from alpaca.trading.enums import AssetClass, AssetStatus, OrderSide, QueryOrderStatus
@@ -11,25 +11,29 @@ from alpaca.trading.models import Asset, Order, TradeAccount
 from alpaca.trading.requests import GetAssetsRequest, GetOrdersRequest
 
 load_dotenv()
-ALPACA_KEY = os.getenv("ALPACA_KEY")
-ALPACA_SECRET = os.getenv("ALPACA_SECRET")
+#ALPACA_KEY = os.getenv("ALPACA_KEY")
+#ALPACA_SECRET = os.getenv("ALPACA_SECRET")
 
-trading_client = TradingClient(ALPACA_KEY, ALPACA_SECRET, paper=True)
+#trading_client = TradingClient(ALPACA_KEY, ALPACA_SECRET, paper=True)
 
-
-def get_acc_info() -> TradeAccount | RawData:
+@with_alpaca_trading_client
+def get_acc_info(trading_client) -> TradeAccount | RawData:
     """Get account info from AlpacaAPI."""
     account = trading_client.get_account()
     return account
 
-
-def search_assets(asset_class: AssetClass | None = None, status: AssetStatus | None = None) -> list[Asset] | RawData:
+@with_alpaca_trading_client
+def search_assets(trading_client: TradingClient, asset_class: AssetClass | None = None, status: AssetStatus | None = None) -> list[Asset] | RawData:
     """Search assets from AlpacaAPI."""
     search_params = GetAssetsRequest(asset_class=asset_class, status=status)
     return trading_client.get_all_assets(search_params)
 
-
-def get_orders_info(status: QueryOrderStatus, side: OrderSide) -> list[Order] | RawData:
+@with_alpaca_trading_client
+def get_orders_info(trading_client: TradingClient, status: QueryOrderStatus = QueryOrderStatus.ALL, side: OrderSide | None = None) -> list[Order] | RawData:
     """Get orders info from AlpacaAPI."""
     request_params = GetOrdersRequest(status=status, side=side)
     return trading_client.get_orders(filter=request_params)
+
+#print(get_acc_info())
+#print(search_assets())
+#print(get_orders_info())

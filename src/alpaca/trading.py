00899cd95cb9de88cd45f822e2b979105e563e46
Vlad Yanovich <vladislavyanovich2004@gmail.com>
2025-07-02 14:14:21 +0300

    created functions in trading.py and added them as tools to mcp.py
    Took 3 hours 1 minute

--- src/alpaca/trading.py
+++ src/alpaca/trading.py
@@ -1,20 +1,12 @@
 """Account manager for Alpaca API."""
 
-import os
-
-from dotenv import load_dotenv
-from common import with_alpaca_trading_client
+from src.alpaca.common import with_alpaca_trading_client
 from alpaca.common import RawData
 from alpaca.trading.client import TradingClient
-from alpaca.trading.enums import AssetClass, AssetStatus, OrderSide, QueryOrderStatus
 from alpaca.trading.models import Asset, Order, TradeAccount
-from alpaca.trading.requests import GetAssetsRequest, GetOrdersRequest
+from alpaca.trading.requests import MarketOrderRequest, LimitOrderRequest, GetAssetsRequest, GetOrdersRequest
+from alpaca.trading.enums import OrderSide, TimeInForce, AssetClass, AssetStatus, QueryOrderStatus
 
-load_dotenv()
-#ALPACA_KEY = os.getenv("ALPACA_KEY")
-#ALPACA_SECRET = os.getenv("ALPACA_SECRET")
-
-#trading_client = TradingClient(ALPACA_KEY, ALPACA_SECRET, paper=True)
 
 @with_alpaca_trading_client
 def get_acc_info(trading_client) -> TradeAccount | RawData:
@@ -23,7 +15,7 @@ def get_acc_info(trading_client) -> TradeAccount | RawData:
     return account
 
 @with_alpaca_trading_client
-def search_assets(trading_client: TradingClient, asset_class: AssetClass | None = None, status: AssetStatus | None = None) -> list[Asset] | RawData:
+def search_all_assets(trading_client: TradingClient, asset_class: AssetClass | None = None, status: AssetStatus | None = None) -> list[Asset] | RawData:
     """Search assets from AlpacaAPI."""
     search_params = GetAssetsRequest(asset_class=asset_class, status=status)
     return trading_client.get_all_assets(search_params)
@@ -34,6 +26,51 @@ def get_orders_info(trading_client: TradingClient, status: QueryOrderStatus = Qu
     request_params = GetOrdersRequest(status=status, side=side)
     return trading_client.get_orders(filter=request_params)
 
-#print(get_acc_info())
-#print(search_assets())
-#print(get_orders_info())
+@with_alpaca_trading_client
+def cancel_open_orders(trading_client: TradingClient):
+    """Cancel all open orders"""
+    cancel_statuses = trading_client.cancel_orders()
+    return cancel_statuses
+
+@with_alpaca_trading_client
+def get_all_positions(trading_client: TradingClient):
+    """Return all open positions"""
+    positions = trading_client.get_all_positions()
+    return positions
+
+@with_alpaca_trading_client
+def close_all_positions(trading_client: TradingClient):
+    """Close all open positions"""
+    cancel_positions = trading_client.close_all_positions(cancel_orders=False)
+    return cancel_positions
+
+"""Creating an Order"""
+
+@with_alpaca_trading_client
+def market_order(trading_client: TradingClient, symbol: str , qty: float, side: OrderSide = OrderSide.BUY, time_in_force: TimeInForce = TimeInForce.DAY):
+    """Buy or sell a stock at the best available price"""
+    market_order_data = MarketOrderRequest(
+        symbol=symbol, # Ticker symbol
+        qty=qty, # Fractional shares
+        side=side, # Buying or selling (Buying by default)
+        time_in_force=time_in_force # Order expires at market close by default
+    )
+    submit_market_order = trading_client.submit_order(
+        order_data=market_order_data
+    )
+    return submit_market_order
+
+@with_alpaca_trading_client
+def limit_order(trading_client: TradingClient, symbol: str , limit_price: float, notional: float, side: OrderSide = OrderSide.BUY, time_in_force: TimeInForce = TimeInForce.GTC):
+    """Buy or sell a stock at a specific price or better"""
+    limit_order_data = LimitOrderRequest(
+        symbol=symbol, # Ticker symbol
+        limit_price=limit_price, # Minimum acceptable price
+        notional=notional, #Amount to sell or buy (buy by default)
+        side=side, # Buying or selling (Buying by default)
+        time_in_force=time_in_force # Fill-or-Kill: Entire order must execute immediately by default
+    )
+    submit_limit_order = trading_client.submit_order(
+        order_data=limit_order_data
+    )
+    return submit_limit_order
--- src/router/mcp/mcp.py
+++ src/router/mcp/mcp.py
@@ -1,9 +1,10 @@
 """Create MCP server with components."""
 
 from mcp.server.fastmcp import FastMCP
+from src.alpaca.trading import market_order, get_acc_info, search_all_assets, get_orders_info, cancel_open_orders, get_all_positions, close_all_positions, limit_order
+from alpaca.trading.enums import OrderSide, TimeInForce
 
 mcp = FastMCP()
-
 
 @mcp.resource("echo://{message}")
 def echo_resource(message: str) -> str:
@@ -21,3 +22,51 @@ def echo_tool(message: str) -> str:
 def echo_prompt(message: str) -> str:
     """Create an echo prompt."""
     return f"Please process this message: {message}"
+
+@mcp.tool()
+def get_account_info():
+    """Access details about brokerage account"""
+    account_info = get_acc_info()
+    return account_info
+
+@mcp.tool()
+def search_assets():
+    """Provide a list of all available assets"""
+    all_assets = search_all_assets()
+    return all_assets
+
+@mcp.tool()
+def get_orders_history():
+    """return all the orders associated with the account."""
+    orders_history = get_orders_info()
+    return orders_history
+
+@mcp.tool()
+def cancel_orders():
+    """Cancel all open orders"""
+    canceled_orders = cancel_open_orders()
+    return canceled_orders
+
+@mcp.tool()
+def get_positions():
+    """Return all open positions"""
+    all_positions = get_all_positions()
+    return all_positions
+
+@mcp.tool()
+def close_positions():
+    """Close all open positions"""
+    closed_positions = close_all_positions()
+    return closed_positions
+
+@mcp.tool()
+def create_market_order(symbol: str , qty: float, side: OrderSide = OrderSide.BUY, time_in_force: TimeInForce = TimeInForce.DAY):
+    """Buy or sell a stock at the best available price"""
+    submit_market_order = market_order(symbol = symbol, qty = qty, side = side, time_in_force = time_in_force)
+    return submit_market_order
+
+@mcp.tool()
+def create_limit_order(symbol: str , limit_price: float, notional: float, side: OrderSide = OrderSide.BUY, time_in_force: TimeInForce = TimeInForce.GTC):
+    """Buy or sell a stock at a specific price or better"""
+    submit_limit_order = limit_order(symbol = symbol, limit_price = limit_price, notional = notional, side = side, time_in_force = time_in_force)
+    return submit_limit_order

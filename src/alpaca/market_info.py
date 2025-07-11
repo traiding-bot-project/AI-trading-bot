9d10031a0b5c9ded83816ed56632829556ccd77f
Andrzej-Tezyk <andrewtezhik@gmail.com>
2025-07-11 23:11:56 +0200

    added news data extractor

--- src/alpaca/market_info.py
+++ src/alpaca/market_info.py
@@ -7,13 +7,11 @@ import dotenv
 from src.alpaca.common import with_alpaca_trading_client
 
 from alpaca.data.historical import CryptoHistoricalDataClient
-from alpaca.data.historical import NewsClient # cool, but looks too slow
-from alpaca.data.historical import OptionHistoricalDataClient
-from alpaca.data.historical import ScreenerClient # 10 most active stocks
+from alpaca.data.historical import NewsClient
 from alpaca.data.historical import StockHistoricalDataClient
 
 from alpaca.data.timeframe import TimeFrame
-from alpaca.data.requests import CryptoBarsRequest, StockBarsRequest
+from alpaca.data.requests import CryptoBarsRequest, StockBarsRequest, NewsRequest
 from alpaca.common.enums import Sort
 from alpaca.data.enums import Adjustment, DataFeed
 
@@ -23,9 +21,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# TODO: other data requests; implement later if needed 
 #client_option = OptionHistoricalDataClient()
-#client_news = NewsClient()
-#client_active_stocks = ScreenerClient()
+#client_active_stocks = ScreenerClient() # 10 most active stocks
 
 
 def get_crypto_market_data (symbol_or_symbols: list, timeframe: TimeFrame, 
@@ -62,6 +60,24 @@ def get_stock_market_data (symbol_or_symbols: list,
                                     sort=sort)
     recieved_data = client_stock.get_stock_bars(request_params)
     return recieved_data.df
+
+
+def get_news_data (start: datetime, 
+                    end: datetime, 
+                    include_content: (Optional[bool]) = True,
+                    exclude_contentless: (Optional[bool]) = False,
+                    limit: (Optional[int])=None, # Limit of news items to be returned for given page.
+                    sort: (Optional[Sort])=Sort.DESC):
+    client_news = NewsClient()(api_key = os.getenv("ALPACA_KEY"), secret_key = os.getenv("ALPACA_SECRET"))
+    request_params = NewsRequest(start=start,
+                                    end=end,
+                                    include_content=include_content,
+                                    exclude_contentless=exclude_contentless,
+                                    limit=limit,
+                                    sort=sort)
+    recieved_data = client_news.get_stock_bars(request_params)
+    return recieved_data.df
+
 # Convert to dataframe
 #print(get_crypto_market_data(["BTC/USD"], TimeFrame.Hour, datetime.datetime(2022, 9, 1), datetime.datetime(2022, 9, 7)))
 #print(get_stock_market_data(["TSLA"], TimeFrame.Hour, datetime.datetime(2022, 9, 1), datetime.datetime(2022, 9, 7)))

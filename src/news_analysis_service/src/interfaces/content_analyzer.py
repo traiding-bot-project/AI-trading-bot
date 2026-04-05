41fbd4197a9ca45f45c8b9000db949d888bf3c9c
Aliaksandr Zaman <81390098+AliaksandrZaman@users.noreply.github.com>
2026-04-06 01:28:08 +0200

    Add available models check for AIContentAnalyzer (#55)

--- src/news_analysis_service/src/interfaces/content_analyzer.py
+++ src/news_analysis_service/src/interfaces/content_analyzer.py
@@ -1,9 +1,18 @@
 """Model for the Ollama service implementation used in the AI Content Analyzer."""
 
-from pydantic import BaseModel, ConfigDict, SkipValidation
+import asyncio
+import logging
+from typing import Self
+
+from pydantic import BaseModel, ConfigDict, SkipValidation, model_validator
 
 from src.interfaces.ai_service import AIService
 from src.models.action_union_types import AnalyzeContentRequest, AnalyzeContentResponse, ListModelsResponse
+
+logger = logging.getLogger(__name__)
+
+
+VALIDATE_AVAILABLE_MODELS_TIMEOUT = 3
 
 
 class AIContentAnalyzer(BaseModel):
@@ -12,6 +21,24 @@ class AIContentAnalyzer(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     service: SkipValidation[AIService]
+
+    @model_validator(mode="after")
+    def vaidate_available_models(self) -> Self:
+        """Check if the underlying AI service has available models."""
+        try:
+            logger.info("Logging available models for AI Content Analyzer")
+            response = asyncio.run(
+                asyncio.wait_for(self.service.list_models(), timeout=VALIDATE_AVAILABLE_MODELS_TIMEOUT)
+            )
+            model_names = [model.name for model in response.models]
+            logger.info(f"Available models for AI Content Analyzer: {model_names}")
+            return self
+        except TimeoutError:
+            logger.error("Timeout while fetching available models for AI Content Analyzer")
+            return self
+        except Exception as e:
+            logger.error(f"Failed to log available models: {e}")
+            return self
 
     async def analyze_content(self, request: AnalyzeContentRequest) -> AnalyzeContentResponse:
         """Analyze content using the underlying AI service based on the given request."""

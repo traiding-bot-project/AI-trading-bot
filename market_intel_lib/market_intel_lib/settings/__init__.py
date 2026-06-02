36987cc6d6f2549a9b1215115953aea4fdfb7b66
Andrzej-Tezyk <andrewtezhik@gmail.com>
2026-06-03 00:53:36 +0200

    feat: init

--- market_intel_lib/market_intel_lib/settings/__init__.py
+++ market_intel_lib/market_intel_lib/settings/__init__.py
@@ -1,11 +1,9 @@
 """Settings module for the Telegram User App Service microservice."""
 
 from market_intel_lib.constants import SETTINGS_PATH
-from src.settings.models.settings_model import Settings
+from market_intel_lib.models.settings import InfiscalSettings
 from market_intel_lib.toml.ingest_toml import load_settings
 
-logger = getLogger(__name__)
-
-settings = load_settings(SETTINGS_PATH, Settings)
+settings = load_settings(SETTINGS_PATH, InfiscalSettings)
 
 __all__ = ["settings"]
